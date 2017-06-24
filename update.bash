b63feed00815fe7891de751b32a47807a52e8382
Thomas Tan <thomastanck@gmail.com>
2017-06-24 10:20:53 +0000

    Make update script pull and push commits

--- update.bash
+++ update.bash
@@ -1,8 +1,20 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+git pull
+
 mv thomastanck.github.io/.git .tmpgit
 rm -rf thomastanck.github.io
 mkdir thomastanck.github.io
 mv .tmpgit thomastanck.github.io/.git
 hugo
+
+cd thomastanck.github.io
+git add .
+git commit -m "Update site."
+git push
+cd ..
+
+git add thomastanck.github.io
+git commit -m "Update site."
+git push
