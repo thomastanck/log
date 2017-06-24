d3db9e39124cdedd3a05a3cfbd3cbd617394f88f
Thomas Tan <thomastanck@gmail.com>
2017-06-24 10:25:54 +0000

    Only one instance of update script will run at a time.

--- update.bash
+++ update.bash
@@ -1,20 +1,25 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-git pull
+(
+	# Ensure only one instance of this script is running
+	flock -n 9 || exit 1
 
-mv thomastanck.github.io/.git .tmpgit
-rm -rf thomastanck.github.io
-mkdir thomastanck.github.io
-mv .tmpgit thomastanck.github.io/.git
-hugo
+	git pull
 
-cd thomastanck.github.io
-git add .
-git commit -m "Update site."
-git push
-cd ..
+	mv thomastanck.github.io/.git .tmpgit
+	rm -rf thomastanck.github.io
+	mkdir thomastanck.github.io
+	mv .tmpgit thomastanck.github.io/.git
+	hugo
 
-git add thomastanck.github.io
-git commit -m "Update site."
-git push
+	cd thomastanck.github.io
+	git add .
+	git commit -m "Update site."
+	git push
+	cd ..
+
+	git add thomastanck.github.io
+	git commit -m "Update site."
+	git push
+) 9>/var/lock/log-hugo-update.lock
