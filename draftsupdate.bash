50cdfdd385eec1f48dfb35b4d57bcabcafc51663
Thomas Tan <thomastanck@gmail.com>
2017-06-25 09:43:47 +0000

    Add drafts update script
    similar to actual update script for manual draft updating

---
+++ draftsupdate.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+(
+	# Ensure only one instance of this script is running
+	flock -n 9 || exit 1
+
+	# In case .bashrc isn't soured e.g. in a noninteractive terminal
+	# This happens when you run a command via `ssh host 'command'`
+	#source ~/.bashrc force
+
+	#git pull
+
+	#mv thomastanck.github.io/.git .tmpgit
+	#rm -rf thomastanck.github.io
+	#mkdir thomastanck.github.io
+	#mv .tmpgit thomastanck.github.io/.git
+	rm -rf public
+	hugo --buildDrafts -d public --baseURL https://log.tomys.cc/
+
+	#cd thomastanck.github.io
+	#git add .
+	#git commit -m "Update site."
+	#git push
+	#cd ..
+
+	#git add thomastanck.github.io
+	#git commit -m "Update site."
+	#git push
+) 9>/var/lock/log-drafts-hugo-update.lock
