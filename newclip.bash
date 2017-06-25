9ae0a79291852af5b60750d9ac2f79b113f3a4f7
Thomas Tan <thomastanck@gmail.com>
2017-06-25 18:05:54 +0800

    newclip script that reads the link from xclip -o

---
+++ newclip.bash
@@ -0,0 +1,17 @@
+cd "$(dirname "$0")"
+link="$(xclip -o)"
+curdate=$(date +%Y_%m_%d)
+timestamp=$(date -u +%FT%TZ)
+filename=content/log/${curdate}_$(wc -w <<<$(ls content/log | grep ${curdate})).md
+#hugo new $filename
+#sed -i -e 's`^title=".*"$`title="'$title'`' content/$filename
+echo '+++' > $filename
+echo 'categories = ["log"]' >> $filename
+echo 'date = "'$timestamp'"' >> $filename
+echo 'tags = ["log"]' >> $filename
+echo 'title = "!#title#!"' >> $filename
+echo 'link = "'$link'"' >> $filename
+echo '' >> $filename
+echo '+++' >> $filename
+echo '' >> $filename
+vim +'execute "normal! /!#title#!\<CR>"' $filename
