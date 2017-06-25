8626b3561c818880ca7be982b8f50a7438a819f2
Thomas Tan <thomastanck@gmail.com>
2017-06-25 18:09:29 +0800

    Automatically start commit after writing log

--- newclip.bash
+++ newclip.bash
@@ -1,10 +1,13 @@
 cd "$(dirname "$0")"
+
 link="$(xclip -o)"
 curdate=$(date +%Y_%m_%d)
 timestamp=$(date -u +%FT%TZ)
 filename=content/log/${curdate}_$(wc -w <<<$(ls content/log | grep ${curdate})).md
+
 #hugo new $filename
 #sed -i -e 's`^title=".*"$`title="'$title'`' content/$filename
+
 echo '+++' > $filename
 echo 'categories = ["log"]' >> $filename
 echo 'date = "'$timestamp'"' >> $filename
@@ -15,4 +18,8 @@ echo '' >> $filename
 echo '+++' >> $filename
 echo '' >> $filename
 echo '<!--more-->' >> $filename
+
 vim +'execute "normal! /!#title#!\<CR>"' $filename
+
+git add $filename
+git commit
--- newlog.bash
+++ newlog.bash
@@ -1,11 +1,14 @@
 cd "$(dirname "$0")"
+
 link="$1"
 [ -z "$1" ] && read link
 curdate=$(date +%Y_%m_%d)
 timestamp=$(date -u +%FT%TZ)
 filename=content/log/${curdate}_$(wc -w <<<$(ls content/log | grep ${curdate})).md
+
 #hugo new $filename
 #sed -i -e 's`^title=".*"$`title="'$title'`' content/$filename
+
 echo '+++' > $filename
 echo 'categories = ["log"]' >> $filename
 echo 'date = "'$timestamp'"' >> $filename
@@ -16,4 +19,8 @@ echo '' >> $filename
 echo '+++' >> $filename
 echo '' >> $filename
 echo '<!--more-->' >> $filename
+
 vim +'execute "normal! /!#title#!\<CR>"' $filename
+
+git add $filename
+git commit
